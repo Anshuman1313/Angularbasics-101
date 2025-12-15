0ef8859ed5e565f541e8c80023a4f421cdbf0bd2
Anshuman1313 <168716686+Anshuman1313@users.noreply.github.com>
2025-12-15 22:05:49 +0530

    Trying array fn
    map,reduce,find and many more

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,112 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import gsap from 'gsap';
+@Component({
+  selector: 'app-arrayfn',
+  templateUrl: './arrayfn.html'
+})
+export class Arrayfn {
+  nums = [0, 15, 20, 25, 30];
+  users = [
+    {name:'A',age:15},
+    {name:'B',age:25},
+    {name:'C',age:5},
+    {name:'D',age:32}
+  ]
+ fruits = ['apple', 'banana', 'guava','orange'];
+
+ FruitsReduce(){
+  let  fruitsObject = this.fruits.reduce((acc,fruit,i)=>{
+    acc[fruit] = i
+   return acc
+}, {} as Record<string, number>);
+console.log(fruitsObject);
+console.log(this.fruits,"this is the original fruits")
+ }
+  SumOfNumbers(){
+    let sum = this.nums.reduce((prev,next)=>{
+      console.log("prev",prev,"next",next);
+      return (next +prev)
+    })
+    console.log("Sum of numbers ",sum)
+
+
+    
+  }
+
+  findUser(){
+    console.log("Finding User the user",this.users)
+    let found = this.users.find((user)=>user.age>18)
+    let found2 = this.users.find(isA)
+
+    function isA (user:{name:string,age:number}){
+      return user.name === 'A'
+    }
+    console.log("if the user is found1",found)
+    console.log("if the user is found2",found2)
+  }
+
+  findevennumber() {
+    console.log("find even num called")
+    this.nums = this.nums.filter((num) => {
+      console.log('checking', num);
+      if(num === 0) console.log("THe number is neither odd nor even")
+      return num % 2 === 0;
+
+    })
+  }
+
+  square() {
+    console.log("nums before ", this.nums)
+    this.nums = this.nums.map((num) => num * num)
+    console.log("nums after ", this.nums)
+  }
+  original() {
+    console.log("i am clicking original")
+    this.nums = [1, 2, 3, 4, 5]
+  }
+}
+
+
+@Component({
+  selector: 'app-user',
+  template: `
+  <div>Username: {{username}} the number is {{number}}</div>
+  <button (click)="increment()">Increment</button>
+  <button (click)="decrement()">Decrement</button>
+  @if (number == 2) {
+  <div> The number is 2</div>
+  }
+  @else {
+    <div> The number is after else statement {{number}}</div>
+    }
+
+  @for (a of users; track a.id){
+  <p >{{a.name}}</p>
+  }
+  <div class="box"> Hi heelo how are you </div>
+  `,
+})
+export class User {
+  username = 'Anshuman';
+  number = 1;
+  users = [{ id: 1, name: "Anshuman" }, { id: 2, name: "Anmol" }, { id: 3, name: "Sushant" }]
+
+  increment() {
+    console.log()
+    this.number++;
+  }
+  decrement() {
+    this.number--;
+  }
+
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [User, Arrayfn]
 })
 export class App {
-  protected readonly title = signal('basics-101');
+  name = 'Anshuman';
 }
