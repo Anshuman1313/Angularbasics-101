005814e683eaa8f978989c3950293a22e767120c
Anshuman1313 <168716686+Anshuman1313@users.noreply.github.com>
2026-01-08 11:04:58 +0530

    get request
    get request

--- src/app/pages/advance/advance.ts
+++ src/app/pages/advance/advance.ts
@@ -1,51 +1,103 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, signal } from '@angular/core';
 import { from, map, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { AsyncPipe } from '@angular/common';
+import { FormControl,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-advance',
-  imports: [],
+  imports: [ReactiveFormsModule ],
   templateUrl: './advance.html',
   styleUrl: './advance.css',
 })
 
 export class Advance {
-  data : any = null;
-  
-  
-  data$ = this.getuser()
-  constructor(private http: HttpClient){
-  //   const data = data$.subscribe(e=>{
-  //   console.log(e,"e");
-  // })
-  // console.log(data,"data")
-  }
-  getuser(){
+  data : any[] = [];
+  // data = signal<any[]>([]);
+  title = "aman";
+  data2 : any[] = [];
+  idControl = new FormControl('')
+
+
+  constructor(private http: HttpClient,private cdr: ChangeDetectorRef){}
+ 
+  // Fetch data based on input ID
+  fetchById() {
+    const id = this.idControl.value;
     
-    this.data = this.http.get('https://api.restful-api.dev/objects')
-    console.log(typeof(this.data))
-    return this.http.get('https://api.restful-api.dev/objects')
+    if (id) {
+      const params = new HttpParams().append("id",id)
+      const paramskey = params.keys()
+      console.log(paramskey,"Paramkeys")
+      console.log("param",params)
+      this.http.get('https://api.restful-api.dev/objects', { params }).subscribe({
+        next: (response: any) => {
+          this.data = response;
+          console.log('Data loaded:', this.data);
+          this.cdr.detectChanges();
+        },
+        error: (error) => console.error('Error:', error)
+      });
+    }
+  }
+
+  // Fetch multiple IDs (comma-separated: 3,5,10)
+  fetchMultipleIds() {
+    const ids = this.idControl.value?.split(',').map(id => id.trim());
+    
+    if (ids && ids.length > 0) {
+      let params = new HttpParams();
+      ids.forEach(id => {
+        params = params.append('id', id);
+      });
+      
+      this.http.get('https://api.restful-api.dev/objects', { params }).subscribe({
+        next: (response: any) => {
+          this.data = response;
+          console.log('Data loaded:', this.data);
+          this.cdr.detectChanges();
+
+        },
+        error: (error) => console.error('Error:', error)
+      });
+    }
+  }
+
+  // Fetch all objects
+  fetchAll() {
+    this.http.get('https://api.restful-api.dev/objects').subscribe({
+      next: (response: any) => {
+        this.data = response;
+        console.log('All data loaded:', this.data);
+          this.cdr.detectChanges();
+
+      },
+      error: (error) => console.error('Error:', error)
+    });
+  }
+
+  getuser(){
+        this.http.get('https://api.restful-api.dev/objects').subscribe({
+      next: (response: any) => {
+        this.data = response;
+        // this.data.set(response);
+      console.log('Data loaded:', this.data);
+      // this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error:', error);
+      }
+    });
+
   } 
+  getUserDummy(){
+    this.data = [{
+      id:1,
+      name : 'asadf'
+    }]
+  }
 
-
- 
-
-  
-
-  
-
-  
-  
-
-
- 
-  // constructor(){
-  //   const number$ = of(1,2,3,4)
-  //   console.log(typeof(number$),"type of number$")
-  //   number$.subscribe(val=>console.log("val",val))
-  //   const mappedNumber =number$.pipe(map(n=>n*2))
-  //   console.log(mappedNumber,"mappedNumber")
-  //   number$.pipe(map(n=>n*2)).subscribe(console.log)
-  // }
+  testclick(){
+    this.title="asdf";
+  }
 }
