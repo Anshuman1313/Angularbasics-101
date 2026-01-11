783ebda709fae40916d7e9f9132dfadada27f31f
Anshuman1313 <168716686+Anshuman1313@users.noreply.github.com>
2026-01-11 15:41:02 +0530

    authguard
    authguard

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -7,14 +7,14 @@ export const authGuard: CanActivateFn = (route, state) => {
    const authService = inject(Auth);
   const router = inject(Router);
 
-  console.log('🛡️ Auth Guard - Checking authentication...');
+  console.log('Auth Guard - Checking authentication...');
   console.log('Is logged in:', authService.isLoggedIn());
 
   if (authService.isLoggedIn()) {
-    console.log('✅ Access granted');
+    console.log('Access granted');
     return true;
   }
 
-  console.log('❌ Access denied - Redirecting to login');
+  console.log('Access denied - Redirecting to login');
   return router.createUrlTree(['/login']);
 };
